34a073dc61059ece660f82792387e16bb0ad93c9
Felipe Tsuyoshi Suzuki <ftsuyoshisuzuki@gmail.com>
2024-02-22 15:59:14 -0300

    Adiciona dependencias de teste

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,13 +61,19 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // test
     testImplementation("junit:junit:4.13.2")
+    testImplementation ("org.mockito:mockito-core:4.11.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0-RC2")
+    testImplementation ("com.google.truth:truth:1.4.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // OpenCSV
     implementation ("com.opencsv:opencsv:5.5.2")
